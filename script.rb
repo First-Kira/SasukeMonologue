fc63dd0bcc153d0a0a9a62c80029016283623019
夜神月 <31050499+First-Kira@users.noreply.github.com>
2024-06-06 17:57:02 -0400

    Add files via upload

---
+++ script.rb
@@ -0,0 +1,6 @@
+x =
+
+"
+my name is sasuke uchiha. I hate a lot of things. And I dont particulary like anything. What I have is not a dream because I will make it a reality. I am going to restore my clan and destroy a certain someone.  
+"
+system("say '#{x}'")
